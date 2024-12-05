dc3326c85fa7c5388171802be68902e629d61c1c
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-12-04 23:35:30 -0300

    refactor: lint code

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,27 +6,23 @@ dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export const auth = (request, response, next) => {
-  const token = request.headers.authorization?.split(" ")[1];
+  const token = request.headers.authorszation?.split(" ")[1];
 
   if (!token) {
-    return response
-      .status(401)
-      .json({
-        statusCode: 401,
-        statusText: "UNATHORIZED",
-        message: "Token não fornecido.",
-      });
+    return response.status(401).json({
+      statusCode: 401,
+      statusText: "UNATHORIZED",
+      message: "Token não fornecido.",
+    });
   }
 
   jwt.verify(token, JWT_SECRET, (error, user) => {
     if (error) {
-      return response
-        .status(403)
-        .json({
-          statusCode: 403,
-          statusText: "FORBIDDEN",
-          message: "Token inválido.",
-        });
+      return response.status(403).json({
+        statusCode: 403,
+        statusText: "FORBIDDEN",
+        message: "Token inválido.",
+      });
     }
     request.user = user;
     next();
