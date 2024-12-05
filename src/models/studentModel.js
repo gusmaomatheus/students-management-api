72599150063cac97502f849914700553d94871ce
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-12-04 23:02:16 -0300

    feat(model): create student model

---
+++ src/models/studentModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+  id: { type: Number, required: true, unique: true },
+  nome: { type: String, required: true },
+  ra: { type: String, required: true, unique: true },
+  nota1: { type: Number, required: true },
+  nota2: { type: Number, required: true },
+});
+
+const Student = mongoose.model("Student", studentSchema);
+export default Student;
